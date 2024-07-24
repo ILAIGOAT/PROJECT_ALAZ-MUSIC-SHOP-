d0efdc0f12506bbafc77b35f6ce75f82eae1e7e9
Itamar <thnr2008@gmail.com>
2024-07-24 16:52:55 +0300

    integrated the sidebar into the main page

--- HomePage/indexscript.js
+++ HomePage/indexscript.js
@@ -1,9 +1,18 @@
 document.getElementById("open").addEventListener("click", function(){
     document.getElementsByClassName("popup")[0].classList.add("active");
 });
+document.getElementById("btn").addEventListener("click", function() {
+  document.getElementsByClassName("popup")[0].classList.add("active");
+});
+document.getElementById("sidelist").addEventListener("click", function() {
+  document.getElementsByClassName("popup")[0].classList.add("active");
+});
 document.getElementById("dismiss-popup-btn").addEventListener("click", function(){
     document.getElementsByClassName("popup")[0].classList.remove("active");
+    document.querySelector('.sidebar').classList.remove("active");
 });
+
+
 
 
 
