edbd65c0c2d4ca9a06efe3b36183ea64e715e6c6
ILAIGOAT <ilai130708@gmail.com>
2024-07-24 14:52:28 +0300

    fa

--- HomePage/indexscript.js
+++ HomePage/indexscript.js
@@ -8,17 +8,5 @@ document.getElementById("dismiss-popup-btn").addEventListener("click", function(
 
 
 
-function ChangeBackground()
-{
 
-  if(document.body.style.background = "#212121")
-  {
-      document.body.style.background = "#e8e8e8";
-      document.body.style.color = "#42424A";
-  }
-
-  
-    
-
-}
 
