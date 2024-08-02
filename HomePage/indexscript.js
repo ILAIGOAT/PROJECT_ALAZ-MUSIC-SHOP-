65ec6e9d279b3b1f031b8121cb3aeb7492e41291
ILAIGOAT <ilai130708@gmail.com>
2024-08-02 20:01:06 +0300

    Update indexscript.js

--- HomePage/indexscript.js
+++ HomePage/indexscript.js
@@ -1,23 +1,20 @@
 
-
-document.getElementById("open").addEventListener("click", function(){
-    document.getElementsByClassName("popup")[0].classList.add("active");
-});
-document.getElementById("btn").addEventListener("click", function() {
-  document.getElementsByClassName("popup")[0].classList.add("active");
-});
-document.getElementById("sidelist").addEventListener("click", function() {
-  document.getElementsByClassName("popup")[0].classList.add("active");
-});
-document.getElementById("dismiss-popup-btn").addEventListener("click", function(){
-    document.getElementsByClassName("popup")[0].classList.remove("active");
-    document.querySelector('.sidebar').classList.remove("active");
-});
-
-
-
-
-
-
-
-
+if(localStorage.getItem('userConnected') === 'false'){
+  document.getElementById("open").href = "#";
+  document.getElementById("open").addEventListener("click", function(){
+      document.getElementsByClassName("popup")[0].classList.add("active");
+  });
+  document.getElementById("btn").addEventListener("click", function() {
+        document.getElementsByClassName("popup")[0].classList.add("active");
+  });
+  document.getElementById("sidelist").addEventListener("click", function() {
+        document.getElementsByClassName("popup")[0].classList.add("active");
+  });
+  document.getElementById("dismiss-popup-btn").addEventListener("click", function(){
+      document.getElementsByClassName("popup")[0].classList.remove("active");
+      document.querySelector('.sidebar').classList.remove("active");
+  });
+}
+else{
+  document.getElementById("open").href = "../shopping cart/shoppingCart.html";
+}
