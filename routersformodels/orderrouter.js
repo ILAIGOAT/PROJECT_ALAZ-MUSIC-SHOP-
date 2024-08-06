93a75751be4030271fccdefd0a24405d7371db2d
ILAIGOAT <ilai130708@gmail.com>
2024-08-06 21:33:11 +0300

    chaNGe

--- routersformodels/orderrouter.js
+++ routersformodels/orderrouter.js
@@ -5,4 +5,34 @@ const Item = require('../models/itemmodel.js');
 var router = express.Router();
 const bcrypt = require('bcrypt');
 
+router.post('/addorder', async (req, res) => {
+    const { address, cart, cartAmounts, totalprice, useremail } = req.body;
+
+    console.log("Received add order request:", req.body);
+
+    try {
+        const order = new Order({
+            address,
+            cart,
+            cartAmounts,
+            totalprice,
+            useremail
+        });
+
+        await order.save();
+        console.log("Product was added successfully:", order);
+
+
+        const user = await User.findOneAndUpdate({email: useremail},{$set: [{cart:[]},{cartAmounts:[]}]});
+        if (!user) {
+            console.log("User not exists:", email);
+            return res.status(422).json({ error: "User not found" });
+        }
+        await user.orders.push(order._id);
+        return res.status(201).json({ message: "Order Made successfully!!" });
+    } catch (error) {
+        console.error("Error during Making New Orders product:", error);
+        return res.status(500).json({ error: 'Server error' });
+    }
+})
 module.exports = router;
--- shopping cart/MakeOrders.js
+++ shopping cart/MakeOrders.js
@@ -3,5 +3,34 @@ function MakeAnOrder()
     tmpprice = document.getElementById("totalPrice").innerHTML;
     totalprice = parseFloat(tmpprice.slice(1));
     address = document.getElementById("useraddress").value;
+    email = sessionStorage.getItem('ConnectedEmail');
+    cart = JSON.parse(sessionStorage.getItem('CartItemIds'));
+    cartAmounts = JSON.parse(sessionStorage.getItem('CartItemAmounts'));
+
+    fetch('http://localhost:88/order/addorder', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            address,
+            cart,
+            cartAmounts,
+            totalprice,
+            useremail: email
+        })
+    })
+    .then(res => res.json())
+    .then(res => {
+        console.log(res);
+        if(res.error) {
+            alert(res.error);
+            console.log("adding error:", res.error);
+        } if(res.message) {
+            alert('Order Done successfully!');
+            console.log("Order Done successfuly:", res.message);
+            window.location.href = "/HomePage/index.html";
+        }
+    });
     
 }
