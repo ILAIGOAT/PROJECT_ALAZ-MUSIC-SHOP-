45a79db64bda3dff4918e84abd29fc7d00a1c9ea
Itamar <thnr2008@gmail.com>
2024-08-07 23:45:07 +0300

    hi

--- DashBoard/helpers.js
+++ DashBoard/helpers.js
@@ -1,12 +1,10 @@
 function UpdateCategoryLine(event){
     var targetElement = event.target;
     
-    // Check if the target element is an <a> or inside an <a> (for nested elements like <i>)
     if (targetElement.tagName !== 'A') {
         targetElement = targetElement.closest('a');
     }
 
-    // Get the innerHTML of the <a> element
     var innerHTML = targetElement.innerHTML;
     console.log(innerHTML);
     sessionStorage.setItem('CategoryClicked',innerHTML);
